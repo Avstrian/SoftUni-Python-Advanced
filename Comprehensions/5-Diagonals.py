5967c2908b8f0e4252494655efe567695c414815
Dimitar Zhelyazkov <dimitarzhelyazkov3003@gmail.com>
2021-06-09 22:05:00 +0300

    Delete 5-Diagonals.py

--- Comprehensions/5-Diagonals.py
+++
@@ -1,4 +0,0 @@
-matrix = [input().split(", ") for j in [i for i in range(0, int(input()))]]
-first_diag = [[num for num in row if int(num) == row] for row in matrix]
-print(first_diag)
-#asdasdsadsa
